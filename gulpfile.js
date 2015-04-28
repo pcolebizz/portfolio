c08dfdb7857de229aa8c3cf7c1ce0625a6057fb3
Peter Cole <pcolebizz@gmail.com>
2015-04-27 23:55:47 -0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,117 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+	concatCss = require('gulp-concat-css');
+	//handlebars = require('gulp-compile-handlebars');
+	
+	gulp.task('styles', function() {
+	  return sass('src/css/master.scss', { style: 'expanded' }).on('error', errorHandler)
+	    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+	    .pipe(gulp.dest('build/css'))
+	    .pipe(rename({suffix: '.min'}))
+	    .pipe(minifycss())
+	    .pipe(gulp.dest('build/css'))
+	    .pipe(notify({ message: 'Styles task complete' }));
+	});
+	
+	
+	gulp.task('scripts', function() {
+	  return gulp.src('src/js/*.js')
+	    //.pipe(jshint('.jshintrc'))
+	    //.pipe(jshint.reporter('default'))
+	    .pipe(concat('all-scripts.js'))
+	    .pipe(gulp.dest('build/js'))
+	    .pipe(rename({suffix: '.min'}))
+	    .pipe(uglify())
+	    .pipe(gulp.dest('build/js'))
+	    .pipe(notify({ message: 'Scripts task complete' }));
+	});
+	
+	//move the js vendor file
+	gulp.task('move-vendor-files', function(){
+		return gulp.src('src/js/vendor/*.js')
+        .pipe(gulp.dest('build/js/vendor'))
+		.pipe(notify({ message: 'Move vendor js files task complete' }));
+	});
+	
+	//compress images
+	gulp.task('images', function() {
+	  return gulp.src('src/img/**/*')
+	    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+	    .pipe(gulp.dest('build/img'))
+	    .pipe(notify({ message: 'Images task complete' }));
+	});
+	
+	//concat js
+	gulp.task('concatjs', function(){
+		return gulp.src('src/js/*.js')
+		.pipe(concat('all-scripts.js'))
+        .pipe(gulp.dest('build/js'))
+		.pipe(notify({ message: 'Concat JS task complete' }));
+	});
+	
+	//concat css
+	gulp.task('concatcss', function () {
+	  gulp.src('src/css/**/*.scss')
+	    .pipe(concatCss("main.scss"))
+	    .pipe(gulp.dest('src/css/'));
+	});
+
+
+	// Clean
+	gulp.task('clean', function(cb) {
+	    del(['build/css', 'build/js'], cb)
+	});
+	
+	// Clean-with images
+	gulp.task('clean-with-images', function(cb) {
+	    del(['build/css', 'build/js', 'build/img'], cb)
+	});
+	
+	// Default task with images
+	gulp.task('default-images', ['clean-with-images'], function() {
+	    gulp.start('styles','scripts', 'images');
+		//gulp.start('watch', 'styles','concatjs','scripts', 'images');
+	});
+
+	// Default task
+	gulp.task('default', ['clean'], function() {
+	    gulp.start('styles','scripts','move-vendor-files');
+		//gulp.start('watch', 'styles','concatjs','scripts', 'images');
+	});
+	
+	
+	gulp.task('watch', function() {
+
+	  // Watch .scss files
+	  gulp.watch('src/css/**/*.scss', ['styles']);
+
+	  // Watch .js files
+	  gulp.watch('src/js/**/*.js', ['scripts']);
+
+	  // Watch image files
+	  gulp.watch('src/img/**/*', ['images']);
+	  
+	  // Create LiveReload server
+	  livereload.listen();
+ 
+	  // Watch any files in dist/, reload on change
+	  gulp.watch(['src/**/*']).on('change', livereload.changed);
+
+	});
+
+	// Handle the error
+	function errorHandler (error) {
+	  console.log("THIS IS IT: " + error.toString());
+	  this.emit('end');
+	}
---
+++ src/js/main.js
@@ -0,0 +1,202 @@
+/**
+ */
+
+function scrollWin(section){
+	$('html,body').animate({
+	scrollTop: $(section).offset().top}, 1200, function(){
+		window.location.hash = section;
+	});
+}
+
+
+function fadeSection(section){
+	//fade in the about section
+	$('#about article').fadeOut('slow', function(){
+		//$('#spinner').fadeOut('slow');
+	});	
+	
+	//fade in the about section
+	$('#about article').fadeIn('slow', function(){
+		//$('#spinner').fadeOut('slow');
+	});		
+}
+
+
+$(document).ready(function(){
+	
+	var currentPage = '#about';
+	var previousPage;
+	var pageHash;
+	handleActiveBtn(currentPage);
+	
+	$('#about-section').click(function(event) {			
+		event.preventDefault();
+		pageHash = $(this).attr('href');		
+		handleActiveBtn('#about');
+		scrollWin("#about");		
+	});
+	
+	$('#work-section').click(function(event) {	
+		event.preventDefault();
+		handleActiveBtn('#work');
+		pageHash = $(this).attr('href');  		
+		scrollWin("#work");
+	});
+	
+	$('#launchbx-section').click(function(event) {
+		event.preventDefault();
+		pageHash = $(this).attr('href');
+		handleActiveBtn('#launchbx');
+		scrollWin("#launchbx");
+	});
+	
+	$('#groovable-section').click(function(event) {
+		event.preventDefault();
+		pageHash = $(this).attr('href');		
+		handleActiveBtn('#groovable');
+		scrollWin("#groovable");
+	});
+	
+	$('#ooto-section').click(function(event) {
+		event.preventDefault();
+		pageHash = $(this).attr('href');
+		handleActiveBtn('#ooto');
+		scrollWin("#ooto");
+	});
+	
+
+	function handleActiveBtn(newBtn){
+		
+		previousPage = currentPage;
+		//alert("prev page: " + previousPage);
+		currentPage = newBtn;
+		//alert("current page: " + currentPage);
+		
+		
+		previousBtn = previousPage + '-section';
+		$(previousBtn).removeClass('active');
+		
+		currentBtn = newBtn + '-section';
+		
+		$(currentBtn).addClass('active');
+		//xcurrentPage = newBtn;
+		
+	}
+	
+						
+	// Cache the Window object
+	$window = $(window);
+	
+	// Cache the Y offset and the speed of each sprite
+	$('[data-type]').each(function() {	
+		$(this).data('offsetY', parseInt($(this).attr('data-offsetY')));
+		$(this).data('Xposition', $(this).attr('data-Xposition'));
+		$(this).data('speed', $(this).attr('data-speed'));
+		$(this).data('id', $(this).attr('id'));
+	});
+	
+	// For each element that has a data-type attribute
+	$('section[data-type="background"]').each(function(){
+
+		// Store some variables based on where we are
+		var $self = $(this),
+			offsetCoords = $self.offset(),
+			topOffset = offsetCoords.top;
+		
+		// When the window is scrolled...
+	    $(window).scroll(function() {
+	
+			// If this section is in view
+			if ( ($window.scrollTop() + $window.height()) > (topOffset) &&
+				 ( (topOffset + $self.height()) > $window.scrollTop() ) ) {
+					
+				$('#device').css('height', '200px');
+	
+				// Scroll the background at var speed
+				// the yPos is a negative value because we're scrolling it UP!								
+				var yPos = -($window.scrollTop() / $self.data('speed')); 
+				
+				// If this element has a Y offset then add it on
+				if ($self.data('offsetY')) {
+					yPos += $self.data('offsetY');
+				}
+				
+				// Put together our final background position
+				var coords = '50% '+ yPos + 'px';
+				
+				var section = '#' + $self.data('id');
+				handleActiveBtn(section);
+				
+				//alert(section);
+
+				// Move the background
+				$self.css('-webkit-transform: translate3d(0,0,0)');
+				$self.css({ backgroundPosition: coords });
+							
+				// Check for other sprites in this section	
+				$('[data-type="sprite"]', $self).each(function() {
+					
+					// Cache the sprite
+					var $sprite = $(this);
+
+					// Use the same calculation to work out how far to scroll the sprite
+					var yPos = -($window.scrollTop() / $sprite.data('speed'));					
+					var coords = $sprite.data('Xposition') + ' ' + (yPos + $sprite.data('offsetY')) + 'px';
+					
+					$sprite.css({ backgroundPosition: coords });	
+
+				}); // sprites
+			
+				/*
+				// Check for any Videos that need scrolling
+				$('[data-type="video"]', $self).each(function() {
+					
+					// Cache the video
+					var $video = $(this);
+					
+					// There's some repetition going on here, so 
+					// feel free to tidy this section up. 
+					var yPos = -($window.scrollTop() / $video.data('speed'));					
+					var coords = (yPos + $video.data('offsetY')) + 'px';
+	
+					$video.css({ top: coords });													
+					
+				}); // video	
+				*/
+			
+			}; // in view
+	
+		$('#device').css('height', '0px');
+		}); // window scroll
+			
+	});	// each data-type
+	
+	
+		//fade in the about section
+		$('#about article').fadeIn('slow', function(){
+			//$('#spinner').fadeOut('slow');
+		});	
+
+		
+		//now load the other images that we were hiding	
+		$('#thelist img').each(function(){
+		  $(this).attr('src', $(this).attr('id'));
+		});
+		
+		imageUrl = "img/clouds1.png";
+		lbxScreen = "img/launcher-screenshot.png";
+		grScreen = "img/groovable-overview.png";
+		ootoScreen = "img/ooto1.png";
+		experScreen = "img/groovable-overview.png";
+		
+		$('#about .clouds').css('background-image', 'url(' + imageUrl + ')');
+		$('#work .clouds').css('background-image', 'url(' + imageUrl + ')');
+		$('#launchbx .clouds').css('background-image', 'url(' + imageUrl + ')');
+		
+		$('#launchbx .screenshot').css('background-image', 'url(' + lbxScreen + ')');
+		$('#groovable .screenshot').css('background-image', 'url(' + grScreen + ')');
+		$('#ooto .screenshot').css('background-image', 'url(' + ootoScreen + ')');
+		$('#experiments .screenshot').css('background-image', 'url(' + experScreen + ')');
+		
+
+}); // document ready
